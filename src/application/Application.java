1f662c9981cfe94c3980a431d6bd34bb125c3219
aitorfi <aitorfidalgo@gmail.com>
2021-01-06 13:22:18 +0100

    First commit. Empty Application class implemented.

---
+++ src/application/Application.java
@@ -0,0 +1,22 @@
+package application;
+
+/**
+ * Contains the main method of the application.
+ * 
+ * @author Aitor Fidalgo
+ */
+public class Application {
+
+    /**
+     * Main method used to execute the sorting algorithms.
+     * 
+     * This method is meant to be empty so that the user of the code can execute
+     * any of the sorting algorithms included in the application and see their performance.
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/application/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Contains the main methods to execute the sorting algorithms and some other
+ * useful methods to play with them.
+ */
+package application;
